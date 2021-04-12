bc778bef62e8ba281d86ecc9ae8ab04db661c108
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-04-12 21:36:37 +0530

    added index+random

--- pset2/encyclopedia/urls.py
+++ pset2/encyclopedia/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.content, name="page"),
+    path("random/", views.random, name="random")
 ]
--- pset2/encyclopedia/views.py
+++ pset2/encyclopedia/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
+from random import choice
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
-    })
 
+    })
+def content(request, title):
+    return render(request, "encyclopedia/page.html",{
+        "title": title
+    })
+def random(request):
+    li = util.list_entries()
+    f = choice(li)
+    print(f)
+    return HttpResponseRedirect(f'/wiki/{f}')
+    
