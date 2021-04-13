2ba3f09e1ad9baf16ee5a123c853f6e9e74a3378
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-04-13 11:10:27 +0530

    added search functionality

--- pset2/encyclopedia/urls.py
+++ pset2/encyclopedia/urls.py
@@ -5,5 +5,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.content, name="page"),
-    path("random/", views.random, name="random")
+    path("random/", views.random, name="random"),
+    path("search/", views.search, name="search")
 ]
--- pset2/encyclopedia/views.py
+++ pset2/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from markdown2 import Markdown
 
 from . import util
 from random import choice
@@ -12,12 +13,38 @@ def index(request):
 
     })
 def content(request, title):
+    if util.get_entry(title)==None:
+        return render(request, "encyclopedia/404.html", {
+            "name":title
+        })
+    a = util.get_entry(title)
+    converter = Markdown()
+    html = converter.convert(a)
+    print(html)
     return render(request, "encyclopedia/page.html",{
-        "title": title
+        "title": title,
+        "content": html
     })
 def random(request):
     li = util.list_entries()
     f = choice(li)
     print(f)
     return HttpResponseRedirect(f'/wiki/{f}')
+def search(request):
+    q = request.GET.get('q')
+    li = util.list_entries()
+    f = []
+    # We redirect user to the page immediately, if the entry matches a page 
+    if q in li:
+        return HttpResponseRedirect(reverse('encyclopedia:page', args=[q]))
+    # It might be a substring - Even if it isn't, {% empty %} handles that
+    for each in li:
+        if q.lower() in each.lower():
+            f+=[each]
+    return render(request, "encyclopedia/search.html",{
+        "f":f
+    })
     
+    
+
+        
